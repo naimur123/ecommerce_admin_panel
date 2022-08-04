99fbbf144044bc5ebcc826ec76685f49d0c87da8
Naimur Rahman <70331302+naimur123@users.noreply.github.com>
2022-08-05 00:51:44 +0600

    heroku google signin

--- config/services.php
+++ config/services.php
@@ -33,7 +33,7 @@ return [
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' => 'http://kenakatalaravel.herokuapp.com/register/google/signin',
+        'redirect' => 'http://127.0.0.1:8000/register/google/signin',
     ],
     // 'heroku' => [
     //     'client_id' => env('HEROKU_CLIENT_ID'),
